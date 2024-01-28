c4177c28596828ecee8fa427fb6ee80c6edca46e
Kevin Loughead <kvnloughead@gmail.com>
2024-01-27 22:58:36 -0500

    docs: add docstring for helper function

--- cmd/api/helper.go
+++ cmd/api/helper.go
@@ -9,10 +9,10 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+// Reads ID param from context and parses it as an int64. If the ID doesn't parse to a positive integer, an error is returned.
 func (app *application) readIdParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 
-	// Parse ID from request params as int64.
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
 	if err != nil || id < 1 {
 		return 0, errors.New("ID must be a positive integer")
