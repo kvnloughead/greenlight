4fc36d356e4f2eb804a01a6ff410acf566a9c724
Kevin Loughead <kvnloughead@gmail.com>
2024-02-04 11:13:57 -0500

    feat: createMovie handler logic

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -9,7 +11,28 @@ import (
 
 // createMovie handles POST requests to the /v1/movies endpoint.
 func (app *application) createMovie(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Movie created\n"))
+	// Struct to store the data from the responses body. The struct's fields must
+	// be exported to use it with json.NewDecoder.
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	// Decode responses body into the struct above. Keys will be mapped to struct
+	// fields with matching json tags, but if that fails an attempt to match the
+	// struct keys (case-insensitively). Unmatched keys in the decoded data will
+	// be ignored. Unmatched keys in the decode destination will be given their
+	// zero value.
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// Dump decoded body as an HTTP response.
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 // showMovie handles GET requests to the /v1/movies/:id endpoint.
