068cc75c81f55783c430a6ab78560d025e475fe2
Kevin Loughead <kvnloughead@gmail.com>
2024-02-05 20:29:19 -0500

    refactor: ValidateMovie helper

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -13,13 +13,8 @@ import (
 // body is decoded by the app.readJSON helper. See that function for details
 // about error handling.
 //
-// Requests must meet the following validation.
-//
-//   - Title, Year, Runtime, and Genres are required.
-//   - Title must be less than 500 bytes.
-//   - Year must be between 1888 and the present.
-//   - Runtime must be a positive integer.
-//   - There must be between 1 and 5 unique genres.
+// Request bodies are validated by ValidateMovie. A failedValidattionResponse
+// error is sent if one or more fields fails validation.
 func (app *application) createMovie(w http.ResponseWriter, r *http.Request) {
 	// Struct to store the data from the responses body. The struct's fields must
 	// be exported to use it with json.NewDecoder.
@@ -36,22 +31,15 @@ func (app *application) createMovie(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	m := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
 	v := validator.New()
-
-	v.Check(input.Title != "", "title", "must be provided")
-	v.Check(len(input.Title) < 500, "title", "must be less than 500 bytes")
-
-	v.Check(input.Year != 0, "year", "must be provided")
-	v.Check(input.Year >= 1888, "year", "must be after 1888")
-	v.Check(input.Year <= int32(time.Now().Year()), "year", "must not be in the future")
-
-	v.Check(input.Runtime != 0, "runtime", "must be provided")
-	v.Check(input.Runtime > 0, "runtime", "must be a positive integer")
-
-	v.Check(input.Genres != nil, "genres", "must be provided")
-	v.Check(len(input.Genres) >= 1, "genres", "must be at least 1 genre")
-	v.Check(len(input.Genres) <= 5, "genres", "must be no more than 5 genres")
-	v.Check(validator.Unique(input.Genres), "genres", "must not contain duplicate values")
+	data.ValidateMovie(v, m)
 
 	if !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"time"
+
+	validator "github.com/kvnloughead/greenlight/internal"
+)
 
 // Movie is a struct representing data for a single movie entry.
 type Movie struct {
@@ -11,4 +15,30 @@ type Movie struct {
 	Runtime   Runtime   `json:"runtime,omitempty"`
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
+}
+
+// ValidateMovie validates the fields of a Movie struct. The fields must meet
+// the following requirements:
+//
+//   - Title, Year, Runtime, and Genres are required.
+//   - Title must be less than 500 bytes.
+//   - Year must be between 1888 and the present.
+//   - Runtime must be a positive integer.
+//   - There must be between 1 and 5 unique genres.
+func ValidateMovie(v *validator.Validator, m *Movie) {
+
+	v.Check(m.Title != "", "title", "must be provided")
+	v.Check(len(m.Title) < 500, "title", "must be less than 500 bytes")
+
+	v.Check(m.Year != 0, "year", "must be provided")
+	v.Check(m.Year >= 1888, "year", "must be after 1888")
+	v.Check(m.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	v.Check(m.Runtime != 0, "runtime", "must be provided")
+	v.Check(m.Runtime > 0, "runtime", "must be a positive integer")
+
+	v.Check(m.Genres != nil, "genres", "must be provided")
+	v.Check(len(m.Genres) >= 1, "genres", "must be at least 1 genre")
+	v.Check(len(m.Genres) <= 5, "genres", "must be no more than 5 genres")
+	v.Check(validator.Unique(m.Genres), "genres", "must not contain duplicate values")
 }
