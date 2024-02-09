13a880a0de66fefffffbc65182976d6038caaf34
Kevin Loughead <kvnloughead@gmail.com>
2024-02-08 23:10:47 -0500

    feat: add migrations for movie table and constraint

---
+++ migrations/000001_create_movies_table.down.sql
@@ -0,0 +1,3 @@
+--- In Postgresql, DROP TABLE also removeds all indexes and constraints
+--- that are associated with the table.
+DROP TABLE IF EXISTS movies;
---
+++ migrations/000002_add_movies_check_constraints.up.sql
@@ -0,0 +1,7 @@
+ALTER TABLE movies ADD CONSTRAINT movies_runtime_check CHECK (runtime > 0);
+
+--- year must be between 1888 and present. 
+--- Note that 'year' must be in single quotes.
+ALTER TABLE movies ADD CONSTRAINT movies_year_check CHECK (year BETWEEN 1888 AND date_part('year', now()));
+
+ALTER TABLE movies ADD CONSTRAINT movies_genres_check CHECK (array_length(genres, 1) BETWEEN 1 AND 5);
