591a2a6cbe70e0eee205b76ac95c3d04f32d8152
Kevin Loughead <kvnloughead@gmail.com>
2024-02-17 10:29:51 -0500

    feat: provide pagination metadata

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -6,6 +6,30 @@ import (
 	validator "github.com/kvnloughead/greenlight/internal"
 )
 
+// Metadata is a struct that contains pagination data.
+type Metadata struct {
+	CurrentPage  int `json:"current_page,omitempty"`
+	PageSize     int `json:"page_size,omitempty"`
+	FirstPage    int `json:"first_page,omitempty"`
+	LastPage     int `json:"last_page,omitempty"`
+	TotalRecords int `json:"total_records,omitempty"`
+}
+
+func calculateMetadata(totalRecords, page, pageSize int) Metadata {
+	if totalRecords == 0 {
+		return Metadata{}
+	}
+
+	// LastPage calculation is equivalent to ceil(totalRecords / pageSize).
+	return Metadata{
+		CurrentPage:  page,
+		PageSize:     pageSize,
+		FirstPage:    1,
+		LastPage:     (totalRecords + pageSize - 1) / pageSize,
+		TotalRecords: totalRecords,
+	}
+}
+
 type Filters struct {
 	Page         int
 	PageSize     int
