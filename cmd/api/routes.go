28b407a13b6159f117d1eef3223b68928194c434
Kevin Loughead <kvnloughead@gmail.com>
2024-02-22 21:13:31 -0500

    feat: add user registration route

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -13,15 +13,22 @@ import (
 // The defined routes are as follows:
 //
 //   - GET    /v1/healthcheck   Show application information.
+//
 //   - GET    /v1/movies				Show details of all movies (or a subset)
+//
 //   - POST   /v1/movies				Create a new movie.
+//
 //   - GET    /v1/movies/:id	  Show details of a specific movie.
+//
 //   - PATCH  /v1/movies/:id		Update details of a specific movie.
+//
 //   - DELETE /v1/movies/:id	  Delete a specific movie.
+//
+//   - POST   /v1/users         Register a new user.
 //
 // This function also sets up custom error handling for scenarios where no
 // route is matched (404 Not Found) and when a method is not allowed for a
-// given route (405 Method Not Allowed), using the custom error handlers //
+// given route (405 Method Not Allowed), using the custom error handlers
 // defined in api/errors.go.
 //
 // Finally, the router is wrapped with the recoverPanic middleware to handle any
@@ -34,11 +41,14 @@ func (app *application) routes() http.Handler {
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheck)
+
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMovies)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovie)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovie)
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovie)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovie)
+
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUser)
 
 	return app.recoverPanic(app.rateLimit(router))
 }
---
+++ cmd/api/users_handlers.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+
+	validator "github.com/kvnloughead/greenlight/internal"
+	"github.com/kvnloughead/greenlight/internal/data"
+)
+
+// registerUser handles POST requests to the /v1/users endpoint. The request
+// body is decoded by the app.readJSON helper. See that function for details
+// about error handling.
+//
+// The request body must contain a name, email, and password. Request bodies
+// are validated by data.ValidateUser. A failedValidationResponse error is sent
+// if one or more fields fails validation, or if the email is a duplicate.
+//
+// A hash is generated from the plaintext password via bcrypt and stored in the
+// DB.
+func (app *application) registerUser(w http.ResponseWriter, r *http.Request) {
+	// Struct to store the data from the responses body. The struct's fields must
+	// be exported to use it with json.NewDecoder.
+	var input struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Copy info from request body into a new user struct. Below, we set the
+	// password by calling the user struct's Password.Set method.
+	user := &data.User{
+		Name:      input.Name,
+		Email:     input.Email,
+		Activated: false,
+	}
+
+	err = user.Password.Set(input.Password)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	// Validate user. Email uniqueness is checked on attempted insert.
+	v := validator.New()
+	data.ValidateUser(v, user)
+
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	// Insert new record into DB, if possible.
+	err = app.models.Users.Insert(user)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrDuplicateEmail):
+			v.AddError("email", "a user with this email address already exists")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	// Write JSON response.
+	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+}
